858a4c0c7bf3b04c81e4cbe4f945c3c1389da3fe
mcuz <itmtn@wintec.ac.nz>
2017-05-09 13:57:28 +1200

    Initial Commit

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Section1Index()
+        {
+            return View("Section1/index");
+        }
+
+
+    }
+}
--- WebApp/Global.asax.cs
+++ WebApp/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
 
 namespace WebApp
 {
@@ -13,6 +14,7 @@ namespace WebApp
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
