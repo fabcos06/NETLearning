b5fbc9697cc9f33acb736d24240e5da76fb71786
mcuz <itmtn@wintec.ac.nz>
2017-05-22 11:48:06 +1200

    added questionaire

---
+++ WebApp/Controllers/QuestionaireController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+
+    //https://www.danylkoweb.com/Blog/3-ways-to-receive-data-from-postbacks-in-aspnet-mvc-C2
+
+    public class QuestionaireController : Controller
+    {
+        // GET: Questionaire
+        [HttpGet]
+        public ActionResult Questionaire()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProcessQuestionaire(string rating, bool friend, bool internet, bool poster, string contactMethod )
+        {
+            ViewBag.ContactMethod = contactMethod;
+            var list = new List<String>();
+            if (friend) list.Add("friend");
+            if (internet) list.Add("internet");
+            if (poster) list.Add("poster");
+            ViewBag.InformMethods = list;
+            return View(); 
+        }
+
+    }
+}
